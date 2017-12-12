47c9f6e5832f964cf137fe3fdf51c05f2b131018
wuwentao <wwu@genowise.com>
2017-12-12 16:14:57 +0800

    - Update favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="shortcut icon" href="/assets/image_filter/images/favicon.ico" />
 
     <title>@section('title') {{ __(config('app.name')) }} - {{ __($title) }} @show</title>
 
